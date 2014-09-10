b7b3d4f4fbba0346510cc7085b841ac5b85294c8
Mark Wolfe <mark@wolfe.id.au>
2014-09-11 09:56:31 +1000

    Finally locating players reliably.

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bitly/go-simplejson"
 	"github.com/davecgh/go-spew/spew"
-	sonos "github.com/ianr0bkny/go-sonos"
 	"github.com/ninjasphere/go-ninja"
+	"github.com/ninjasphere/go-ninja/devices"
 	"github.com/ninjasphere/go-ninja/logger"
+	"github.com/ninjasphere/go-sonos"
+	"github.com/ninjasphere/go-sonos/ssdp"
 )
 
 const driverName = "driver-sonos"
@@ -19,46 +23,105 @@ const (
 	NetworkInterface = "wlan0"
 )
 
-var log = logger.GetLogger(driverName)
+var nlog = logger.GetLogger(driverName)
+
+func NewPlayer(bus *ninja.DriverBus, sonosUnit *sonos.Sonos) (*devices.MediaPlayerDevice, error) {
+
+	group, _ := sonosUnit.GetZoneGroupAttributes()
+
+	id := group.CurrentZoneGroupID
+	name := group.CurrentZoneGroupName
+
+	nlog.Infof("Making media player with ID: %s Label: %s", id, name)
+
+	sigs, _ := simplejson.NewJson([]byte(`{
+			"ninja:manufacturer": "Sonos",
+			"ninja:productName": "Sonos Player",
+			"ninja:productType": "MediaPlayer",
+			"ninja:thingType": "MediaPlayer"
+	}`))
+
+	deviceBus, err := bus.AnnounceDevice(id, "media-player", name, sigs)
+	if err != nil {
+		nlog.FatalError(err, "Failed to create media player device bus")
+	}
+
+	player, err := devices.CreateMediaPlayerDevice(name, deviceBus)
+
+	if err != nil {
+		nlog.FatalError(err, "Failed to create media player device")
+	}
+
+	player.ApplyTogglePlay = func() error {
+		return sonosUnit.Play(0, "1")
+	}
+
+	return player, nil
+}
+
+func detectZP() (zonePlayers ssdp.DeviceMap, err error) {
+
+	nlog.Infof("loading discovery mgr")
+	mgr, err := sonos.Discover(NetworkInterface, DiscoveryPort)
+	if nil != err {
+		return
+	}
+
+	zonePlayers = make(ssdp.DeviceMap)
+	for uuid, device := range mgr.Devices() {
+		if device.Product() == "Sonos" && device.Name() == "ZonePlayer" {
+			zonePlayers[uuid] = device
+		}
+	}
+	return
+}
 
 func main() {
-	log.Infof("Starting %s", driverName)
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	nlog.Infof("Starting %s", driverName)
 
 	conn, err := ninja.Connect("com.ninjablocks.sonos")
 	if err != nil {
-		log.HandleError(err, "Could not connect to MQTT")
+		nlog.HandleError(err, "Could not connect to MQTT")
 	}
 
 	pwd, _ := os.Getwd()
 
-	_, err = conn.AnnounceDriver("com.ninjablocks.sonos", driverName, pwd)
+	bus, err := conn.AnnounceDriver("com.ninjablocks.sonos", driverName, pwd)
 	if err != nil {
-		log.HandleError(err, "Could not get driver bus")
+		nlog.HandleError(err, "Could not get driver bus")
 	}
 
 	statusJob, err := ninja.CreateStatusJob(conn, driverName)
 
 	if err != nil {
-		log.HandleError(err, "Could not setup status job")
+		nlog.HandleError(err, "Could not setup status job")
 	}
 
 	statusJob.Start()
 
-	mgr, err := sonos.Discover(NetworkInterface, DiscoveryPort)
-
-	if err != nil {
-		log.HandleError(err, "Could not configure ssdp manager")
-	}
-
+	nlog.Infof("loading reactor")
 	reactor := sonos.MakeReactor(NetworkInterface, EventingPort)
 
+	zonePlayers, err := detectZP()
 	if err != nil {
-		log.HandleError(err, "Could not configure reactor")
+		nlog.HandleError(err, "Error detecting Sonos ZonePlayers")
 	}
 
-	sonosUnits := sonos.ConnectAny(mgr, reactor, sonos.SVC_CONTENT_DIRECTORY|sonos.SVC_AV_TRANSPORT|sonos.SVC_RENDERING_CONTROL)
+	nlog.Infof("Detected %d Sonos ZonePlayers", len(zonePlayers))
 
-	log.Infof(spew.Sprintf("found sonos units %v", sonosUnits))
+	for zone, player := range zonePlayers {
+		nlog.Infof(spew.Sprintf("Found %s %v", zone, player))
+
+		unit := sonos.Connect(player, reactor, sonos.SVC_AV_TRANSPORT|sonos.SVC_ZONE_GROUP_TOPOLOGY|sonos.SVC_MUSIC_SERVICES)
+
+		dev, err := NewPlayer(bus, unit)
+		if err != nil {
+			nlog.HandleError(err, "failed to register media player")
+		}
+
+		nlog.Infof(spew.Sprintf("created media player device %v", dev))
+	}
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
