76c7ec5328d79428d6f9002e5164d9e8ac82e41d
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-18 11:43:56 +1000

    Enable basic media channel (artist/album info etc)

--- driver.go
+++ driver.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
+	"regexp"
+	"time"
+
 	"github.com/bitly/go-simplejson"
 	"github.com/ninjasphere/go-ninja"
 	"github.com/ninjasphere/go-ninja/channels"
 	"github.com/ninjasphere/go-ninja/devices"
 	"github.com/ninjasphere/go-ninja/logger"
 	"github.com/ninjasphere/go-sonos"
+	"github.com/ninjasphere/go-sonos/didl"
 	"github.com/ninjasphere/go-sonos/upnp"
 )
 
@@ -97,25 +103,134 @@ func (sp *sonosPlayer) applyMuted(muted bool) error {
 	return sp.player.UpdateMutedState(muted)
 }
 
-func (sp *sonosPlayer) bindMethods() {
+func (sp *sonosPlayer) applyPlayURL(url string, queue bool) error {
+	return fmt.Errorf("Playing a URL has not been implemented yet.")
+}
+
+func (sp *sonosPlayer) bindMethods() error {
 
 	sp.player.ApplyPlayPause = sp.applyPlayPause
 	sp.player.ApplyStop = sp.applyStop
 	sp.player.ApplyPlaylistJump = sp.applyPlaylistJump
 	sp.player.ApplyVolume = sp.applyVolume
 	sp.player.ApplyMuted = sp.applyMuted
+	sp.player.ApplyPlayURL = sp.applyPlayURL
 
-	sp.player.EnableControlChannel([]string{
+	err := sp.player.EnableControlChannel([]string{
 		"playing",
 		"paused",
 		"stopped",
 		"idle",
 	})
+	if err != nil {
+		return err
+	}
 
-	sp.player.EnableVolumeChannel()
+	err = sp.player.EnableVolumeChannel()
+	if err != nil {
+		return err
+	}
+
+	err = sp.player.EnableMediaChannel()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var timeDuration = regexp.MustCompile("([0-9]{1,2})\\:([0-9]{2})\\:([0-9]{2})")
+
+func parseDuration(t string) (*time.Duration, error) {
+
+	found := timeDuration.FindAllStringSubmatch(t, -1)
+
+	if found == nil || len(found) == 0 {
+		return nil, fmt.Errorf("Failed to parse duration from '%s'", t)
+	}
+
+	duration, err := time.ParseDuration(fmt.Sprintf("%sh%sm%ss", found[0][1], found[0][2], found[0][3]))
+
+	if found == nil || len(found) == 0 {
+		return nil, fmt.Errorf("Failed to parse duration from '%s': %s", t, err)
+	}
+
+	return &duration, nil
+}
+
+func (sp *sonosPlayer) updateMedia() error {
+	t := nlog
+
+	positionInfo, err := sp.GetPositionInfo(0)
+	if err != nil {
+		return err
+	}
+
+	if positionInfo.TrackMetaData == "" {
+		t.Infof("No track!")
+		err = sp.player.UpdateMusicMediaState(nil, nil)
+		return err
+	}
+
+	duration, err := parseDuration(positionInfo.TrackDuration)
+	if err != nil {
+		return err
+	}
+
+	durationMs := int(*duration / time.Millisecond)
+
+	position, err := parseDuration(positionInfo.RelTime)
+	if err != nil {
+		return err
+	}
+
+	positionMs := int(*position / time.Millisecond)
+
+	var trackMetadata didl.Lite
+
+	err = xml.Unmarshal([]byte(positionInfo.TrackMetaData), &trackMetadata)
+	if err != nil {
+		return err
+	}
+
+	//spew.Dump("DIDL", trackMetadata)
+
+	track := &channels.MusicTrackMediaItem{
+		ID:    &positionInfo.TrackURI,
+		Title: &trackMetadata.Item[0].Title[0].Value,
+		Album: &channels.MediaItemAlbum{
+			Name: trackMetadata.Item[0].Album[0].Value,
+		},
+		Artists: &[]channels.MediaItemArtist{
+			channels.MediaItemArtist{
+				Name: trackMetadata.Item[0].Creator[0].Value,
+			},
+		},
+		Duration: &durationMs,
+	}
+
+	err = sp.player.UpdateMusicMediaState(track, &positionMs)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (sp *sonosPlayer) updateState() error {
+
+	/*mediaInfo, err := sp.GetMediaInfo(defaultInstanceID)
+
+	if err != nil {
+		return err
+	}*/
+
+	//func (d *MediaPlayerDevice) UpdateMusicMediaState(item *MusicTrackMediaItem, position *int) error {
+	err := sp.updateMedia()
+
+	if err != nil {
+		return fmt.Errorf("Failed to update current media: %s", err)
+	}
 
 	muted, err := sp.GetMute(defaultInstanceID, upnp.Channel_Master)
 
@@ -176,7 +291,10 @@ func NewPlayer(bus *ninja.DriverBus, sonosUnit *sonos.Sonos) (*sonosPlayer, erro
 
 	sp := &sonosPlayer{sonosUnit, logger.GetLogger("sonosPlayer"), player}
 
-	sp.bindMethods()
+	err = sp.bindMethods()
+	if err != nil {
+		nlog.FatalError(err, "Failed to bind channels to sonos device")
+	}
 
 	err = sp.updateState()
 
