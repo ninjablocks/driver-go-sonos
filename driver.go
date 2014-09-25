ed5c0a6d69a336b57ffcd67a4ac7d7c427e9b650
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-25 10:26:15 +1000

    Don't supply network interface to go-sonos

--- driver.go
+++ driver.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"time"
-
-	"github.com/davecgh/go-spew/spew"
 	"github.com/ninjasphere/go-ninja/api"
 	"github.com/ninjasphere/go-ninja/logger"
 	"github.com/ninjasphere/go-ninja/model"
@@ -15,7 +10,10 @@ import (
 	"github.com/ninjasphere/go-sonos/upnp"
 )
 
-const discoveryInterval = time.Second * 20
+const (
+	DiscoveryPort = 13104
+	EventingPort  = 13105
+)
 
 var info = ninja.LoadModuleInfo("./package.json")
 
@@ -48,13 +46,11 @@ func StartSonosDriver() {
 		d.log.Fatalf("Failed to export driver: %s", err)
 	}
 
-	intName, err := GetInterface()
-
 	if nil != err {
 		nlog.HandleError(err, "Could not locate interface to bind to")
 	}
 
-	d.reactor = sonos.MakeReactor(intName, EventingPort)
+	d.reactor = sonos.MakeReactor(EventingPort)
 
 	d.conn = conn
 }
@@ -79,10 +75,8 @@ func (d *sonosDriver) discover() {
 	} else {
 		for uuid, device := range zonePlayers {
 			if _, ok := d.players[uuid]; !ok {
-				d.log.Infof("Found a new Sonos ZonePlayer: %v", device)
 
 				unit := sonos.Connect(device, d.reactor, sonos.SVC_RENDERING_CONTROL|sonos.SVC_AV_TRANSPORT|sonos.SVC_ZONE_GROUP_TOPOLOGY|sonos.SVC_MUSIC_SERVICES)
-
 				player, err := NewPlayer(d, d.conn, unit)
 
 				if err != nil {
@@ -99,16 +93,8 @@ func (d *sonosDriver) discover() {
 
 func (d *sonosDriver) discoverZonePlayers() (zonePlayers ssdp.DeviceMap, err error) {
 
-	intName, err := GetInterface()
-
-	//intName = "en0"
-
-	if nil != err {
-		return nil, err
-	}
-
 	d.log.Infof("loading discovery mgr")
-	mgr, err := sonos.Discover(intName, DiscoveryPort)
+	mgr, err := sonos.Discover(DiscoveryPort)
 	if nil != err {
 		return
 	}
@@ -128,37 +114,4 @@ func (d *sonosDriver) GetModuleInfo() *model.Module {
 
 func (d *sonosDriver) SetEventHandler(sendEvent func(event string, payload interface{}) error) {
 	d.sendEvent = sendEvent
-}
-
-func GetInterface() (intName string, err error) {
-
-	ifaces, err := net.Interfaces()
-
-	spew.Dump(ifaces, err)
-
-	if err != nil {
-		fmt.Errorf("Failed to get interfaces: %s", err)
-		return
-	}
-
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			fmt.Errorf("Failed to get addresses: %s", err)
-			return "", err
-		}
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPNet:
-				if addr.String() != "127.0.0.1/8" && addr.String() != "::1/128" {
-					intName = i.Name
-				}
-			default:
-				fmt.Printf("unexpected type %T val %v", v, v)
-			}
-
-		}
-	}
-
-	return
 }
--- main.go
+++ main.go
@@ -9,11 +9,6 @@ import (
 	"github.com/ninjasphere/go-ninja/logger"
 )
 
-const (
-	DiscoveryPort = "13104"
-	EventingPort  = "13105"
-)
-
 var nlog = logger.GetLogger(info.Name)
 
 func main() {
@@ -21,56 +16,6 @@ func main() {
 	log.SetFlags(log.Ltime | log.Lshortfile)
 
 	StartSonosDriver()
-	/*nlog.Infof("Starting")
-
-	conn, err := ninja.Connect("com.ninjablocks.sonos")
-	if err != nil {
-		nlog.HandleError(err, "Could not connect to MQTT")
-	}
-
-	pwd, _ := os.Getwd()
-
-	err := conn.ExportDriver()
-	if err != nil {
-		nlog.HandleError(err, "Could not get driver bus")
-	}
-
-	statusJob, err := ninja.CreateStatusJob(conn, driverName)
-
-	if err != nil {
-		nlog.HandleError(err, "Could not setup status job")
-	}
-
-	statusJob.Start()
-
-	intName, err := GetInterface()
-
-	if nil != err {
-		nlog.HandleError(err, "Could not locate interface to bind to")
-	}
-
-	nlog.Infof("loading reactor")
-	reactor := sonos.MakeReactor(intName, EventingPort)
-
-	zonePlayers, err := detectZP()
-	if err != nil {
-		nlog.HandleError(err, "Error detecting Sonos ZonePlayers")
-	}
-
-	nlog.Infof("Detected %d Sonos ZonePlayers", len(zonePlayers))
-
-	for zone, player := range zonePlayers {
-		nlog.Infof(spew.Sprintf("Found %s %v", zone, player))
-
-		unit := sonos.Connect(player, reactor, sonos.SVC_RENDERING_CONTROL|sonos.SVC_AV_TRANSPORT|sonos.SVC_ZONE_GROUP_TOPOLOGY|sonos.SVC_MUSIC_SERVICES)
-
-		_, err := NewPlayer(bus, unit)
-		if err != nil {
-			nlog.HandleError(err, "failed to register media player")
-		}
-
-		nlog.Infof("created media player device %s %s", player.UUID(), player.Name())
-	}*/
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
