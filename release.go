b11b3b14f0c385e62d6d8152db5a9dc299e32889
Mark Wolfe <mark@wolfe.id.au>
2015-03-04 10:00:36 +1100

    Removed keys from source code.

--- release.go
+++ release.go
@@ -4,17 +4,15 @@ package main
 
 import (
 	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	"github.com/ninjasphere/go-ninja/logger"
 )
 
-func debug(string, ...interface{}) {}
+var (
+	BugsnagKey = "" // set by build procedure
+)
 
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.INFO)
-
 	bugsnag.Configure(bugsnag.Configuration{
-		APIKey:       "1548235564b07e5e70bfc794db0cedc9",
+		APIKey:       BugsnagKey,
 		ReleaseStage: "production",
 	})
 }
